abedc7160a1ead99f61664ddaa5e8e6226d1fa23
sergisa <phebra@yandex.ru>
2021-02-18 01:00:53 +0300

    Центровка

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-document.getElementById('simple_button').addEventListener('click', function(e){
-    console.log(e);
-});
 document.getElementById('submitting_stopped_button').onclick = function(e){
     console.log("simple clicking");
     return false;
