627006239387751b52d38b9997e07e5a7d79b884
sergisa <phebra@yandex.ru>
2021-02-18 20:26:59 +0300

    конструирование всплывающего сообщения

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('block_link').onclick = function(){
+
+    document.getElementById("main_popup").classList.add('open');
+    return false;
+};
+
+document.getElementById('site_link').onclick = function(){
+    return false;
+}
