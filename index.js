1a61e2f7ef86f3a50495cc6d32f04eac33d66c3f
sergisa <phebra@yandex.ru>
2021-02-18 15:50:30 +0300

    Привязка событий и действий над окном

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('open_modal').onclick = function(){
+    document.getElementById('main_modal').style.display = "block";
+    return false;
+};
+
+document.getElementById('close_modal').onclick = function(){
+    document.getElementById('main_modal').style.display = "none";
+}
