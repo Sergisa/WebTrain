ab24cf9ccf4b814362fb4a877fc658dd3c06025f
sergisa <phebra@yandex.ru>
2021-02-19 01:23:24 +0300

    Обёртка
    Формирование обёртки и анимация всплытия

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+HTMLElement.prototype.wrap = function(wrapper){
+  
+    this.parentNode.insertBefore(wrapper, this);
+    wrapper.appendChild(this);
+}
+var wrapper = document.createElement('div');
+wrapper.id = 'main_wrapper';
+wrapper.classList.add("wrapper");
+document.getElementById("main_popup").wrap(wrapper);
 document.getElementById('block_link').onclick = function(){
-
-    document.getElementById("main_popup").classList.add('open');
+    document.getElementById("main_wrapper").classList.add('open');
+    setTimeout(function(){
+        document.getElementById("main_wrapper").classList.remove('open');
+    }, 2000);
     return false;
 };
-
-document.getElementById('site_link').onclick = function(){
-    return false;
-}
