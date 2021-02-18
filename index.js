17bc08fb5a95da3cb0b1a62bf87dbcdbb43981c0
sergisa <phebra@yandex.ru>
2021-02-18 15:56:46 +0300

    Обработка нажатия на слой фона

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ document.getElementById('open_modal').onclick = function(){
     document.getElementById('main_modal').style.display = "block";
     return false;
 };
-
+function close_modal(modal){
+    modal.style.display = "none";
+}
 document.getElementById('close_modal').onclick = function(){
-    document.getElementById('main_modal').style.display = "none";
+    close_modal(document.getElementById('main_modal'));
+}
+document.getElementById('main_modal').onclick = function(){
+    close_modal(document.getElementById('main_modal'));
 }
