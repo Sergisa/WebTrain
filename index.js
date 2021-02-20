acaeca12cf113341ae6a6c8170ba63929cdafed7
sergisa <phebra@yandex.ru>
2021-02-21 01:31:40 +0300

    Стеклянное модальное окно
    Модальное прозрачное окно с функциями открытия и закрытия.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 document.getElementById('open_modal').onclick = function(){
     document.getElementById('main_modal').style.display = "block";
     return false;
@@ -11,3 +12,17 @@ document.getElementById('close_modal').onclick = function(){
 document.getElementById('main_modal').onclick = function(){
     close_modal(document.getElementById('main_modal'));
 }
+=======
+function closeModal(){
+  document.getElementById('main_overlay').style.display='none';
+}
+function openModal(){
+  document.getElementById('main_overlay').style.display='flex';
+}
+document.getElementById('main_overlay').onclick = closeModal;
+document.getElementById('close').onclick = closeModal;
+
+document.getElementById('open').onclick = function(){
+  openModal();
+}
+>>>>>>> Stashed changes
