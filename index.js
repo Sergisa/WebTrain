a6dfc4a532e9e816dcd3f2fac7aee9a79600a42a
sergisa <phebra@yandex.ru>
2021-02-21 11:15:14 +0300

    Конструирование модели Toast

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-HTMLElement.prototype.wrap = function(wrapper){
-  
-    this.parentNode.insertBefore(wrapper, this);
-    wrapper.appendChild(this);
-}
-var wrapper = document.createElement('div');
-wrapper.id = 'main_wrapper';
-wrapper.classList.add("wrapper");
-document.getElementById("main_popup").wrap(wrapper);
 document.getElementById('block_link').onclick = function(){
-    document.getElementById("main_wrapper").classList.add('open');
-    setTimeout(function(){
-        document.getElementById("main_wrapper").classList.remove('open');
-    }, 2000);
+    Toast.makeText("Уведомление", Toast.LONG_DELAY).show();
     return false;
 };
