e6a5636c8fc836539748993d068a26a246eb9f62
Sergisa <phebra@yandex.ru>
2021-03-22 18:31:20 +0300

    Исправлены ошибки в коде

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-document.getElementById('open_modal').onclick = function(){
-    document.getElementById('main_modal').style.display = "block";
-    return false;
-};
-function close_modal(modal){
-    modal.style.display = "none";
-}
-document.getElementById('close_modal').onclick = function(){
-    close_modal(document.getElementById('main_modal'));
-}
-document.getElementById('main_modal').onclick = function(){
-    close_modal(document.getElementById('main_modal'));
-}
-=======
 function closeModal(){
   document.getElementById('main_overlay').style.display='none';
 }
@@ -25,4 +10,3 @@ document.getElementById('close').onclick = closeModal;
 document.getElementById('open').onclick = function(){
   openModal();
 }
->>>>>>> Stashed changes
