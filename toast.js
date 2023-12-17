83114512c946938ef39d2337fb80cbc7c9159ad9
Sergisa <phebra@yandex.ru>
2023-12-17 19:18:21 +0300

    Добавление описания

--- toast.js
+++ toast.js
@@ -1,32 +1,31 @@
-HTMLElement.prototype.wrap = function(wrapper){
-  wrapper.appendChild(this);
+HTMLElement.prototype.wrap = function (wrapper) {
+    wrapper.appendChild(this);
 }
 
-function Toast(text, duration){
-  this.currentDuration = duration;
-  this.message = text;
+function Toast(text, duration) {
+    this.currentDuration = duration;
+    this.message = text;
 
-  this.wrapper = document.createElement('div');
-  this.wrapper.id = 'main_wrapper';
-  this.wrapper.classList.add("wrapper");
+    this.wrapper = document.createElement('div');
+    this.wrapper.id = 'main_wrapper';
+    this.wrapper.classList.add("wrapper");
 
-
-  document.body.append(this.wrapper);
-  this.mainPopup = document.createElement('div');
-  this.mainPopup.classList.add("popup");
-  this.mainPopup.wrap(this.wrapper);
-  this.mainPopup.innerHTML = this.message;
-
+    document.body.append(this.wrapper);
+    this.mainPopup = document.createElement('div');
+    this.mainPopup.classList.add("popup");
+    this.mainPopup.wrap(this.wrapper);
+    this.mainPopup.innerHTML = this.message;
 }
+
 Toast.LONG_DELAY = 3500; // 3.5 seconds
 Toast.SHORT_DELAY = 2000; // 2 seconds
-Toast.makeText = function(text, duration){
-  return new Toast(text, duration)
+Toast.makeText = function (text, duration) {
+    return new Toast(text, duration)
 }
-Toast.prototype.show = function(){
-  var _obj = this;
-  this.wrapper.classList.add('open');
-  setTimeout(function(){
-      _obj.wrapper.classList.remove('open');
-  }, this.currentDuration);
+Toast.prototype.show = function () {
+    var _obj = this;
+    this.wrapper.classList.add('open');
+    setTimeout(function () {
+        _obj.wrapper.classList.remove('open');
+    }, this.currentDuration);
 }
